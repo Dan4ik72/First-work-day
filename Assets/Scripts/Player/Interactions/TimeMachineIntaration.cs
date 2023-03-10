ad2cf79faf731155c295bb4dee86bd7831d24cc3
Ivan <dilkas@bk.ru>
2023-03-10 18:46:45 +0400

    Test Scene

---
+++ Assets/Scripts/Player/Interactions/TimeMachineIntaration.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeMachineIntaration : Interactable
+{
+    public override void OnInteract()
+    {
+        Debug.Log("Time Travel");
+    }
+}
