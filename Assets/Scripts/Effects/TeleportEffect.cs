8bc8f827a923026978d995bc2f1e661419b975ab
Ivan <dilkas@bk.ru>
2023-03-12 21:16:35 +0400

    Update

---
+++ Assets/Scripts/Effects/TeleportEffect.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TeleportEffect : MonoBehaviour
+{
+    [SerializeField] private RectTransform _image;
+
+    private void Update()
+    {
+        
+    }
+}
